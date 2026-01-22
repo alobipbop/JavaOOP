c51626e2fba5da73f5334499288035209e2481cb
Tran Linh <linh20061210@gmail.com>
2026-01-22 15:28:18 +0700

    bài 9

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) {
         int b=0, a=1; 
-        for(int i=0; i<45; i++){
-            System.out.println(b + ' ');
+        for(int i=0; i<44; i++){
+            System.out.print(b );
             int c=b+a;
             b=a;
             a=c;
